990751a901b0e3ae9371257eacd99558d4abb755
jan201102 <lukas7876@gmail.com>
2022-03-26 17:39:21 +0100

    Signals update for json-config compatibility

--- signals/signals.py
+++ signals/signals.py
@@ -4,10 +4,17 @@ from signals.Isignals import Isignals
 
 
 class Signals(Isignals):
-    def __init__(self):
-        self.lightName = "Regal/setRGBW"
+    def __init__(self,**config):
+        if "host" in config.keys():
+            self.host = config["host"]
+        else:
+            self.host = "192.168.2.107"
+        if "light" in config.keys():
+            self.lightName = config["light"]
+        else:
+            self.lightName = "Regal/setRGBW"
         self.value = "000000ff"
-        self.host = "192.168.2.107"
+
 
     def activate(self):
         logging.debug("lighting up Signal")
