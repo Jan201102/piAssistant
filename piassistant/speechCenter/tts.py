87930baee517ef4a68c979dd1a9db72d553b6f48
Jan201102 <lukas7876@gmail.com>
2024-09-16 22:17:17 +0200

    upgrade from pytts3 to pyttsx4

--- piassistant/speechCenter/tts.py
+++ piassistant/speechCenter/tts.py
@@ -1,14 +1,15 @@
-import pyttsx3
+import pyttsx4
 import logging
 
 class TTS():
     def __init__(self):
-        self.engine = pyttsx3.init()
+        self.engine = pyttsx4.init()
         self.engine.setProperty('voice', 'german')
         self.engine.setProperty('rate', 150)
 
     def say(self, text):
         logging.debug(f"saying:{text}")
         self.engine.say(text)
+        logging.debug("processing text to be spoken")
         self.engine.runAndWait()
         logging.debug("Done")
